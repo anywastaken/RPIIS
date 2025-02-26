0ad3d9c57a8e5065bd01f8beb2583e4f88ed5014
vinovad <advaskan@gmail.com>
2025-02-26 12:37:37 +0300

    main.cpp

--- sem2/lab2/main.cpp
+++ sem2/lab2/main.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
 #include <string>
-#include "mnozestvo.h"
+#include "mn.h"
 #include <fstream>
 using namespace std;
+
+
 int main() {
     setlocale(0, "rus");
-    int a, b;
-    int* arrA ;
-    int* arrB ;
+    int** sets = nullptr;  
+    int* sizes = nullptr; 
+    int numSets = 0; 
     int choice;
-    
-    
-   
-    load(arrA,a,arrB,b, "D:\\Уник\\Пиовис\\lab2\\soxr.txt");
+    load(sets, sizes, numSets, "D:\\Уник\\Пиовис\\laba2\\soxr.txt");
     while (true) {
         cout << "Меню:" << endl;
-        cout << "1) Добавить множество А" << endl;
-        cout << "2) Добавить множество B" << endl;
-        cout << "3) Просмотр множество А" << endl;
-        cout << "4) Просмотр множество B" << endl;
-        cout << "5) Просмотр симетрической разности" << endl;
-        cout << "6) Сохранить файл" << endl;
-        cout << "7) Закрыть программу" << endl;
+        cout << "1) Добавить множества " << endl;
+        cout << "2) Просмотр множеств " << endl;
+        cout << "3) Просмотр симетрической разности" << endl;
+        cout << "4) Сохранить файл" << endl;
+        cout << "5) Закрыть программу" << endl;
         cout << "Выберите пункт меню: ";
         cin >> choice;
 
         if (choice == 1) {
-            dobavA(arrA, a);      
+            createSets(sets,  sizes, numSets);
         }
+       
         else if (choice == 2) {
-           dobavB(arrB, b); 
-        }
-        else if (choice == 3) {
 
-            cout << "A="; vyvod(arrA, a);
+            vyvod(sets, sizes, numSets);
+        }
+       
+        else if (choice == 3) {
+            int resultSize; 
+            int* result = symDifference(sets, sizes, numSets, resultSize); // Вызов функции
+
+           
+            cout << "Симметрическая разность={ ";
+            for (int i = 0; i < resultSize; ++i) {
+                cout << result[i] << " ";
+            }
+            cout <<"}" <<endl;
+
+            delete[] result;
         }
         else if (choice == 4) {
-
-            cout << "B="; vyvod(arrB, b);
+           
+            save(sets, sizes, numSets, "D:\\Уник\\Пиовис\\laba2\\soxr.txt");
         }
         else if (choice == 5) {
-            
-            symmetricDifference(arrA, a, arrB, b);
-        }
-        else if (choice == 6) {
-            save(arrA, a, arrB, b, "D:\\Уник\\Пиовис\\lab2\\soxr.txt");
-        }
-        else if (choice == 7) {
 
             cout << "Программа закрыта." << endl;
             return 0;
@@ -56,10 +58,14 @@ int main() {
         }
     }
     return 0;
-   
 
-    delete[] arrA;
-    delete[] arrB;
+    
+    for (int i = 0; i < numSets; ++i) {
+        delete[] sets[i];
+    }
+    delete[] sets;
+    delete[] sizes;
+
     return 0;
 }
 
