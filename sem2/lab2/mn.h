13cb16c2810f946cc2e6ce064ebe6a043472c346
vinovad <advaskan@gmail.com>
2025-02-28 20:25:02 +0300

    mn.h

--- sem2/lab2/mn.h
+++ sem2/lab2/mn.h
@@ -1,16 +1,1 @@
-#ifndef mn_h
-#define mn_h
 
-#include <iostream>
-#include <string>
-
-using namespace std;
-void vvod(int* arr, int size);
-bool check(int input, int max);
-void dobavM(int*& arr, int& size, char setName);
-void createSets(int**& sets, int*& sizes, int& numSets);
-int* symDifference(int** sets, int* sizes, int numSets, int& resultSize);
-void vyvod(int** sets, int* sizes, int numSets);
-void save(int** sets, int* sizes, int numSets, const string& filename);
-void load(int**& sets, int*& sizes, int& numSets, const string& filename);
-#endif
