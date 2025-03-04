dd87a5b874ec72d96c438117884ce099bca49cb4
Артём Перфоратор <artemmarudin1@gmail.com>
2025-03-04 19:54:37 +0300

    Update spisok.h

--- sem2/lab1/spisok.h
+++ sem2/lab1/spisok.h
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+void SufArray(string s, vector<string>& suf, vector<int>& pos);
+int SuffShift(string s);
+string MaxPreff(string s, string s1, string s2);
 
