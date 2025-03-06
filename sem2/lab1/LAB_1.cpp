6088b88c11c549b8a32b55a6a9198f0d21033de3
Moolfee <65678342+Moolfee@users.noreply.github.com>
2025-03-06 13:21:33 +0300

    Update LAB_1.cpp

--- sem2/lab1/LAB_1.cpp
+++ sem2/lab1/LAB_1.cpp
@@ -25,11 +25,25 @@ TEST(ArrTest, BinSearch) {
     EXPECT_EQ(index, 2);
 }
 
+TEST(ArrTest, EmpBinSearch) {
+    IntArray arr({ 1, 2, 3, 4, 5 });
+    int index = arr.binarySearch(6);
+    EXPECT_EQ(index, -1);
+}
+
 TEST(ArrTest, Merge) {
     IntArray arr1({ 1, 3, 5 });
     IntArray arr2({ 2, 4, 6 });
     vector<int> merged = arr1.mergeWith(arr2);
     vector<int> expected = { 1, 2, 3, 4, 5, 6 };
+    EXPECT_EQ(merged, expected);
+}
+
+TEST(ArrTest, EmpMerge) {
+    IntArray arr1({ });
+    IntArray arr2({ 2, 4, 6 });
+    vector<int> merged = arr1.mergeWith(arr2);
+    vector<int> expected = {2, 4, 6};
     EXPECT_EQ(merged, expected);
 }
 
@@ -40,3 +54,12 @@ TEST(ArrTest, Intersection) {
     vector<int> expected = { 3, 5 };
     EXPECT_EQ(intersection, expected);
 }
+
+TEST(ArrTest, EmpIntersection) {
+    IntArray arr1({ 1, 3, 5, 7 });
+    IntArray arr2({ });
+    vector<int> intersection = arr1.intersectWith(arr2);
+    vector<int> expected = { };
+    EXPECT_EQ(intersection, expected);
+}
+
